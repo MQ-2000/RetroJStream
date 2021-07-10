20aa913b3d4145063ec9ad12d8044e4f08deaa62
FranX1024 <melissa2000.k@hotmail.com>
2021-07-10 21:27:06 +0200

    Basic backend fd

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Working!')
+const postgresql = require('postgresql');
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+app.use('/', express.static(__dirname + '/client'));
+
+
+
+app.listen(port, function() {
+	console.log(`Listening on port ${port}`);
+});
